547554859e095ac28ad34543b8a74b69abb77815
yahwd <unavoidable1007@gmail.com>
2020-12-09 21:34:02 +0800

    Update Result.js

--- js/Result.js
+++ js/Result.js
@@ -1,2 +1,20 @@
-alert('測驗結果');
+// alert('測驗結果');
+
+
+var main = function() {
+    var ResultPhotos = [
+      '../img/R_Music.png',
+      '../img/R_Old.png',
+      '../img/R_Pop.png',
+      '../img/R_Rock.png',
+    ];
+    $(document).ready(function() {
+        var rP = Math.floor(Math.random() * ResultPhotos.length);
+        $("<img src=" + ResultPhotos[rP] + ">").appendTo('#rPhoto');
+        console.log(ResultPhotos[rP]);
+      // return false;
+      
+    });
+  }
   
+  $(document).ready(main);
